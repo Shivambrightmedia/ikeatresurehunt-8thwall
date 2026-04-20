095431bd5d2d4d62363cad6563a14bba95d23486
shhiiivm <shhiiivm@users.noreply.github.com>
2026-04-20 15:02:28 +0530

    chore: update supabase credentials

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  SUPABASE_URL: 'https://bafnhfmuzkndvpmwrwho.supabase.co',
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhZm5oZm11emtuZHZwbXdyd2hvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyNzA3MjEsImV4cCI6MjA4Nzg0NjcyMX0.UsyXv7oNkhMQc48lqybDSPk0CNTwFg4sqEfumhMwinQ',
+  SUPABASE_URL: 'https://oqscywfbayobehceaquz.supabase.co',
+  SUPABASE_KEY: 'sb_publishable_kALNSWOqdCr2p9IRuqpElQ_oEJe1z2N',
   TOTAL_CLUES: 3,
   COUPON_CODE: 'SCAN-IKEA-2024'
 };
